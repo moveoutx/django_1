736cd0c0163018cedefc8d1432474b368a215083
Mikhail Ivanov <moveout@yandex.ru>
2022-11-20 22:04:54 +0300

    added

---
+++ app/views.py
@@ -0,0 +1,34 @@
+import datetime
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse("time"),
+        'Показать содержимое рабочей директории': reverse("workdir")
+    }
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.datetime.now().time()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    list_work = []
+    path = '.'
+    rez = os.listdir(path)
+    for n in rez:
+        list_work.append(f'{n}, ')
+    return HttpResponse(list_work)
+    # raise NotImplemented
